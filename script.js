9ad5d1538c58293ab724f49cb4f69173097e278c
sandeepkumarbv <31333912+sandeepkumarbv@users.noreply.github.com>
2024-04-06 22:03:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('click-me');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
