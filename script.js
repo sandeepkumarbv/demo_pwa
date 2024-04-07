2533b46f9ef918a388b24a962877a1f47d94baea
sandeepkumarbv <31333912+sandeepkumarbv@users.noreply.github.com>
2024-04-07 15:53:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById('click-me');
-    button.addEventListener('click', function() {
-        alert('Button clicked!');
-    });
-});
